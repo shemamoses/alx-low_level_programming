fa61d8745aff7b44c591ad39c55b3dbc8d2c9563
ALVINdimpos <alvindimpos@gmail.com>
2022-08-23 16:07:53 +0200

    updated

--- 0x0A-python-inheritance/6-base_geometry.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Module for BaseGeometry class"""
-
-
-class BaseGeometry:
-    """Class with public instance method 'area()'"""
-    def area(self):
-        """Raises exception"""
-        raise Exception('area() is not implemented')
